161207d8693684adb4a6dd81eddc837faefa2d5b
Razvan Onofrei <razwan.me@gmail.com>
2017-05-03 12:28:46 +0300

    Fix site identity options to display site title - closes fixes #149

--- header/templates/branding.php
+++ header/templates/branding.php
@@ -5,7 +5,7 @@
  * @see        https://pixelgrade.com
  * @author        Pixelgrade
  * @package    Components/Header
- * @version     1.0.1
+ * @version     1.0.2
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -13,23 +13,29 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 <div <?php pixelgrade_css_class( 'header nav', 'header navbar zone branding wrapper' ); ?>>
+
 	<div class="c-branding">
 
-		<?php if ( function_exists( 'the_custom_logo' ) ) {
-			the_custom_logo();
-		} ?>
+		<div class="c-branding__logo">
+			<?php if ( function_exists( 'the_custom_logo' ) ) {
+				the_custom_logo();
+			} ?>
 
-		<?php if ( function_exists( 'pixelgrade_the_custom_logo_transparent' ) ) {
-			pixelgrade_the_custom_logo_transparent();
-		} ?>
+			<?php if ( function_exists( 'pixelgrade_the_custom_logo_transparent' ) ) {
+				pixelgrade_the_custom_logo_transparent();
+			} ?>
+		</div>
 
-	    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-			<?php if ( is_front_page() && is_home() ) : ?>
-	            <h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
-			<?php else : ?>
-	            <p class="site-title h1"><?php bloginfo( 'name' ); ?></p>
-			<?php endif; ?>
-	    </a>
+		<div class="c-branding__title">
+		    <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+				<?php if ( is_front_page() && is_home() ) : ?>
+		            <h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
+				<?php else : ?>
+		            <p class="site-title h1"><?php bloginfo( 'name' ); ?></p>
+				<?php endif; ?>
+		    </a>
+		</div>
+
 	    <p class="site-description site-description-text"><?php bloginfo( 'description' ) /* WPCS: xss ok. */ ?></p>
 	</div><!-- .c-branding -->
 </div>
