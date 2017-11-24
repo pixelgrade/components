06d5d3ddb6b1bb247182cc12abd34c6749473a24
Victor Panaite <victor.panaite@pixelgrade.com>
2017-11-24 17:00:00 +0200

    Closes #274 [Customizer] Live Preview Issues

--- base/ts/services/global.service.ts
+++ base/ts/services/global.service.ts
@@ -1,6 +1,22 @@
 import * as Rx from 'rx-dom';
 
+interface ExtendedWindow extends Window {
+  wp?: any;
+}
+
 export class GlobalService {
+
+  public static onCustomizerChange(): Rx.Observable<JQuery> {
+    const exWindow: ExtendedWindow = window;
+
+    return Rx.Observable.create( ( observer ) => {
+      if ( exWindow.wp && exWindow.wp.customize ) {
+        exWindow.wp.customize.selectiveRefresh.bind( 'partial-content-rendered', (placement) => {
+          observer.onNext($(placement.container));
+        });
+      }
+    });
+  }
 
   public static onReady(): Rx.Observable<UIEvent> {
     return Rx.DOM.ready();
