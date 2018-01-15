7bbfc13f55aa9968c518664c86482760b55d0c12
Vlad Olaru <vlad.olaru@pixelgrade.com>
2018-01-15 22:57:48 +0200

    added entry header template parts for archives and search

---
+++ blog/template-parts/entry-header-search.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template part used for displaying the entry header for search results pages.
+ *
+ * This template part can be overridden by copying it to a child theme or in the same theme
+ * by putting it in the root `/template-parts/entry-header-search.php` or in `/template-parts/blog/entry-header-search.php`.
+ * @see pixelgrade_locate_component_template_part()
+ *
+ * HOWEVER, on occasion Pixelgrade will need to update template files and you
+ * will need to copy the new files to your child theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see        https://pixelgrade.com
+ * @author     Pixelgrade
+ * @package    Components/Blog
+ * @version    1.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+?>
+
+<header class="entry-header">
+	<h2 class="entry-title  u-page-title-color  h2"><?php printf( esc_html__( 'Search Results for: %s', 'components_txtd' ), '<span>' . get_search_query() . '</span>' ); ?></h2>
+	<?php get_search_form(); ?>
+</header><!-- .entry-header -->
