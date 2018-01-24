4dfecee826e101ebc49e27fbc8a554204fca416a
Razvan <razwan.me@gmail.com>
2018-01-24 13:52:40 +0200

    Merge branch 'bobo' into 15-toolbar

--- header/template-parts/header.php
+++ header/template-parts/header.php
@@ -68,3 +68,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 		</div><!-- .o-wrapper  .u-container-width -->
 	</div><!-- .u-header-sides-spacing -->
 </header><!-- #masthead .site-header -->
+
+<?php get_template_part( 'template-parts/toolbar' ); ?>
+<?php get_template_part( 'template-parts/search-overlay' ); ?>
