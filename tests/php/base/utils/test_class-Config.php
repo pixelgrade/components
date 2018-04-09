f530e13970763eac34d13ed58cf8a822361ccba0
Vlad Olaru <vlad.olaru@pixelgrade.com>
2018-04-09 09:14:21 +0300

    fixed composer.json

---
+++ tests/php/base/utils/test_class-Config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class CP_Tests_Class_Config
+ *
+ * @package Components
+ */
+
+/**
+ * Test base component class Config.
+ *
+ * @group base
+ */
+class CP_Tests_Class_Config extends WP_UnitTestCase {
+
+	/**
+	 * @covers Pixelgrade_Config::hasPageTemplate
+	 */
+	function test_hasPageTemplate() {
+	}
+}
