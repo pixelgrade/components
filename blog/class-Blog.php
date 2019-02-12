9336ec40e6c8cde857643c518f5673ea5c84515e
Vlad Olaru <vlad.olaru@pixelgrade.com>
2019-02-12 09:59:30 +0200

    Merge branch 'dev' of https://github.com/pixelgrade/felt into dev

--- blog/class-Blog.php
+++ blog/class-Blog.php
@@ -833,6 +833,8 @@ class Pixelgrade_Blog extends Pixelgrade_Component {
 
 		// Enqueue the frontend assets
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueueScripts' ) );
+		// We will put this script inline since it is so small.
+		add_action( 'wp_print_footer_scripts', array( $this, 'skip_link_focus_fix' ) );
 
 		// Setup how things will behave in the WP admin area
 		add_action( 'admin_init', array( $this, 'adminInit' ) );
@@ -875,12 +877,29 @@ class Pixelgrade_Blog extends Pixelgrade_Component {
 	 */
 	public function enqueueScripts() {
 		// Register the general frontend styles and scripts specific to blog
-		wp_enqueue_script( 'pixelgrade-navigation', pixelgrade_get_theme_file_uri( trailingslashit( PIXELGRADE_COMPONENTS_PATH ) . trailingslashit( self::COMPONENT_SLUG ) . 'js/navigation.js' ), array(), '20180101', true );
-		wp_enqueue_script( 'pixelgrade-skip-link-focus-fix', pixelgrade_get_theme_file_uri( trailingslashit( PIXELGRADE_COMPONENTS_PATH ) . trailingslashit( self::COMPONENT_SLUG ) . 'js/skip-link-focus-fix.js' ), array(), '20180101', true );
+		wp_register_script( 'pixelgrade-navigation', pixelgrade_get_theme_file_uri( trailingslashit( PIXELGRADE_COMPONENTS_PATH ) . trailingslashit( self::COMPONENT_SLUG ) . 'js/navigation.js' ), array(), '20180101', true );
+		wp_enqueue_script( 'pixelgrade-navigation' );
 
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
 		}
+	}
+
+	/**
+	 * Fix skip link focus in IE11.
+	 *
+	 * This does not enqueue the script because it is tiny and because it is only for IE11,
+	 * thus it does not warrant having an entire dedicated blocking script being loaded.
+	 *
+	 * @link https://git.io/vWdr2
+	 */
+	public function skip_link_focus_fix() {
+		// The following is minified via `terser --compress --mangle -- js/skip-link-focus-fix.js`.
+		?>
+<script>
+	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
+</script>
+		<?php
 	}
 
 	/**
