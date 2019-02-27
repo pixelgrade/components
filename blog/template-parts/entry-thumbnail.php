9008219be1d8dc88236d7ae8b8b8ed62701f2835
Vlad Olaru <vlad.olaru@pixelgrade.com>
2019-02-27 19:27:04 +0200

    refactoring for only outputting the blobs and stamp in pro version

--- blog/template-parts/entry-thumbnail.php
+++ blog/template-parts/entry-thumbnail.php
@@ -23,12 +23,49 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
+// We first need to know the bigger picture - the location this template part was loaded from
+$location = pixelgrade_get_location();
 ?>
 
 <?php if ( has_post_thumbnail() ) { ?>
 
+	<?php
+	/**
+	 * pixelgrade_before_entry_thumbnail hook.
+	 */
+	do_action( 'pixelgrade_before_entry_thumbnail', $location );
+	?>
+	<!-- pixelgrade_before_entry_thumbnail -->
+
 	<div class="entry-thumbnail">
-		<div><?php the_post_thumbnail( 'pixelgrade_single_' . pixelgrade_get_post_thumbnail_aspect_ratio_class() ); ?></div>
-	</div>
+		<div>
+
+			<?php
+			/**
+			 * pixelgrade_before_entry_thumbnail_content hook.
+			 */
+			do_action( 'pixelgrade_before_entry_thumbnail_content', $location );
+			?>
+			<!-- pixelgrade_before_entry_thumbnail_content -->
+
+			<?php the_post_thumbnail( 'pixelgrade_single_' . pixelgrade_get_post_thumbnail_aspect_ratio_class() ); ?>
+
+			<?php
+			/**
+			 * pixelgrade_after_entry_thumbnail_content hook.
+			 */
+			do_action( 'pixelgrade_after_entry_thumbnail_content', $location );
+			?>
+			<!-- pixelgrade_after_entry_thumbnail_content -->
+		</div>
+	</div><!-- .entry-thumbnail -->
+
+	<?php
+	/**
+	 * pixelgrade_after_entry_thumbnail hook.
+	 */
+	do_action( 'pixelgrade_after_entry_thumbnail', $location );
+	?>
+	<!-- pixelgrade_after_entry_thumbnail -->
 
 <?php } ?>
