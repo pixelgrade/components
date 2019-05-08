f6872da5771937ae67451701c50af8c1603af4c1
Vlad Olaru <vlad.olaru@pixelgrade.com>
2019-05-08 15:52:05 +0300

    Added comments.php

--- blog/templates/comments.php
+++ blog/templates/comments.php
@@ -53,8 +53,7 @@ if ( post_password_required() ) {
 
 		<?php
 		// You can start editing here -- including this comment!
-		if ( have_comments() ) :
-		?>
+		if ( have_comments() ) { ?>
 
 			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
 				<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
@@ -81,29 +80,28 @@ if ( post_password_required() ) {
 				?>
 			</ol><!-- .comment-list -->
 
-			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) { // Are there comments to navigate through? ?>
 				<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
 					<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', '__components_txtd' ); ?></h2>
 					<div class="nav-links">
 
-						<div class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', '__components_txtd' ) ); ?></div>
-						<div class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', '__components_txtd' ) ); ?></div>
+						<div
+							class="nav-previous"><?php previous_comments_link( esc_html__( 'Older Comments', '__components_txtd' ) ); ?></div>
+						<div
+							class="nav-next"><?php next_comments_link( esc_html__( 'Newer Comments', '__components_txtd' ) ); ?></div>
 
 					</div><!-- .nav-links -->
 				</nav><!-- #comment-nav-below -->
-				<?php
-			endif; // Check for comment navigation.
-
-		endif; // Check for have_comments().
+			<?php }
+		}
 
 
 		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-		?>
+		if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) { ?>
 
 			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', '__components_txtd' ); ?></p>
 			<?php
-		endif;
+		}
 
 		$args = array(
 			'class_form'    => 'comment-form  inputs--alt',
